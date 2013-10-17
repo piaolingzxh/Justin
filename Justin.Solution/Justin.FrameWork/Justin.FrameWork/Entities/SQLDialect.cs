1f9d88f656e9bc015d6f459e66aa2de3d6c8595b
piaolingzxh <piaolingzxh@qq.com>
2013-10-17 17:03:45 +0800

    SQLDialect.cs

---
+++ Justin.Solution/Justin.FrameWork/Justin.FrameWork/Entities/SQLDialect.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Justin.FrameWork.Entities
+{
+    public enum SQLDialect
+    {
+        [XmlEnum("generic")]
+        Generic,
+        [XmlEnum("access")]
+        Access,
+        [XmlEnum("db2")]
+        Db2,
+        [XmlEnum("derby")]
+        Derby,
+        [XmlEnum("firebird")]
+        Firebird,
+        [XmlEnum("hsqldb")]
+        Hsqldb,
+        [XmlEnum("mssql")]
+        Mssql,
+        [XmlEnum("mysql")]
+        Mysql,
+        [XmlEnum("oracle")]
+        Oracle,
+        [XmlEnum("postgres")]
+        Postgres,
+        [XmlEnum("sysbase")]
+        Sysbase,
+        [XmlEnum("teradata")]
+        Teradata,
+        [XmlEnum("ingres")]
+        Ingres,
+        [XmlEnum("infobright")]
+        Infobright,
+        [XmlEnum("luciddb")]
+        Luciddb,
+        [XmlEnum("vertica")]
+        Vertica,
+        [XmlEnum("neoview")]
+        Neoview,
+        [XmlEnum("greenplum")]
+        Greenplum,
+        [XmlEnum("vectorwise")]
+        Vectorwise,
+        [XmlEnum("hive")]
+        Hive,
+    }
+}
